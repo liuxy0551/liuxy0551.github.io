a0e7036602994aee2d3c55e4f1d079224ab229cb
liuyi <liuyi@dtstack.com>
2024-06-17 11:19:34 +0800

    Site updated: 2024-06-17 11:19:33

--- README.md
+++
@@ -1,7 +0,0 @@
-# 刘先玉
-
-个人空间 https://liuxianyu.cn
-
-Github：https://github.com/liuxy0551
-
-欢迎你的到来~
--- assets/posts/vue-wepy-filter/date.js
+++
@@ -1,9 +0,0 @@
-export default function (date, dateFormat = '%Y-%M-%d %H:%m:%s', useStr = '') {
-  if (date) {
-    if (!(date instanceof Date)) {
-      date = new Date(date.replace('GMT', useStr))
-    }
-    return date.strftime(dateFormat)
-  }
-  return ''
-}
